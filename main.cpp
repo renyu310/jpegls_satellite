3a6b0b3c9025094467016abb7317ea918162d4af
zhangrenyu <zhang.yuren@hotmail.com>
2016-11-16 14:31:02 +0800

    mpi complete

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include <mpi.h>
+#include <mpi.h>
 #include <fstream>
 #include "JpegLs.h"
 #include "BinaryIO.h"
@@ -11,10 +11,10 @@ bool checkFormat(BinaryInputStream &in);
 bool readJLSFrame(BinaryInputStream &in,Options &opt);
 
 //调用该函数文件指针会重置到文件头
-void findSofPostion(BinaryInputStream &in, vector<streampos> &pos);
+void findSosPostion(BinaryInputStream &in, vector<streampos> &pos);
 bool writeOutHeader(ofstream &outf,int rows,int cols,int maxval);
 
-int main(int argc, char **argv)
+int main1(int argc, char **argv)
 {
 
 	string conf = "conf.txt";
@@ -45,7 +45,7 @@ int main(int argc, char **argv)
 	if (!readJLSFrame(in, opt)){ cout << "unknown frame header!"<< endl; return 1; }
 
 	vector<streampos> pos;
-	findSofPostion(in, pos);
+	findSosPostion(in, pos);
 	cout << pos.size() << endl;
 
 	ofstream createOutFile(outfile.c_str(), ios::binary | ios::out);
@@ -77,79 +77,165 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-//int main(int argc, char** argv)
-//{
-//	int rank;
-//	int nodes;
-//
-//	MPI_Init(&argc, &argv);
-//	MPI_Comm_size(MPI_COMM_WORLD, &nodes);
-//	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//
-//	//输入参数
-//	string conf = "conf.txt";
-//	Options opt(conf);
-//	if (rank == 0)
-//	{
-//		opt.showConf();
-//		cout << endl << "------------------------" << endl << endl;
-//	}
-//	
-//	
-//	//获取
-//	string infile = opt.inFile;
-//	string outfile = opt.ouFile;
-//
-//	ifstream ifs(infile, ios::binary | ios::in);
-//	if (!ifs.is_open())
-//	{
-//		cout << "Input file: " << infile << " open failure" << endl;
-//		return 1;
-//	}
-//	BinaryInputStream in(ifs.rdbuf(), opt.ENDIAN);
-//
-//
-//	//testDecoder();
-//
-//	Uint16 str;
-//	if (!checkFormat(in)){ cout << "unknown format!" << endl; return 1; }
-//	if (!readJLSFrame(in, opt)){ cout << "unknown frame header!" << endl; return 1; }
-//
-//	vector<streampos> pos;
-//	findSofPostion(in, pos);
-//	cout << pos.size() << endl;
-//
-//	ofstream createOutFile(outfile, ios::binary | ios::out);
-//	createOutFile.close();
-//
-//	ofstream ofs(outfile, ios::binary | ios::out | ios::in);
-//	if (!ofs.is_open())
-//	{
-//		cout << "Output file: " << outfile << " open failure" << endl;
-//		return 1;
-//	}
-//	writeOutHeader(ofs, opt.ROWS*pos.size(), opt.COLOMNS, pow(2, opt.BITS) - 1);
-//	BinaryOutputStream out(ofs.rdbuf(), BigEndian);
-//
-//
-//
-//	for (int i = 0; i < pos.size(); i++)
-//	{
-//		in.seekg(pos[i]);
-//		decoder(opt, in, out);
-//		cout << "component " << i + 1 << "decoder complete" << endl;
-//	}
-//
-//	ifs.close();
-//	ofs.close();
-//	cout << "success!" << endl;
-//
-//
-//	cout << "Hello:" << nodes << "," << rank << endl;
-//
-//	MPI_Finalize();
-//	return 0;
-//}
+int main(int argc, char** argv)
+{
+	int rank;
+	int nodes;
+	int namelen;
+	char processorName[MPI_MAX_PROCESSOR_NAME];
+
+	double t1, t2, t3, t4, t5, t6, t7;
+
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &nodes);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Get_processor_name(processorName, &namelen);
+
+	if (rank == 0)
+	{
+		t1 = MPI_Wtime();
+		cout << "Program start!" << endl;
+	}
+	
+
+	//输入参数
+	string conf = "conf.txt";
+	Options opt(conf);
+	if (rank == 0)
+	{
+		opt.showConf();
+		cout << endl << "------------------------" << endl << endl;
+	}
+	
+	
+	//获取
+	//string infile = opt.inFile;
+	//string outfile = opt.ouFile;
+
+	opt.ENDIAN = BigEndian;
+	opt.ROWS = 256;
+	opt.COLOMNS = 256;
+	opt.BASIC_T1 = 3;
+	opt.BASIC_T2 = 7;
+	opt.BASIC_T3 = 21;
+	opt.BITS = 10;
+
+	string infile = "data/parall/testt16.jls";
+	string outfile = "data/parall/mpitest.dat";
+
+	if (rank == 0)
+	{
+		t2 = MPI_Wtime();
+		cout << "read input config:"<<t2 - t1 << endl;
+	}
+
+
+	//打开输入文件
+	ifstream ifs(infile.c_str(), ios::binary | ios::in);
+	if (!ifs.is_open())
+	{
+		cout << "Input file: " << infile << " open failure" << endl;
+		return 1;
+	}
+	BinaryInputStream in(ifs.rdbuf(), opt.ENDIAN);
+
+	//检查文件格式和头参数
+	Uint16 str;
+	if (!checkFormat(in)){ cout << "unknown format!" << endl; return 1; }
+	if (!readJLSFrame(in, opt)){ cout << "unknown frame header!" << endl; return 1; }
+	
+	if (rank == 0)
+	{
+		t3 = MPI_Wtime();
+		cout << "input data header check and read:" << t3 - t2 << endl;
+	}
+
+	//找到每个独立块位置
+	vector<streampos> pos;
+	findSosPostion(in, pos);
+	cout << pos.size() << endl;
+
+	if (rank == 0)
+	{
+		t4 = MPI_Wtime();
+		cout << "find add sos position:" << t4 - t3 << endl;
+	}
+
+	if (rank == 0)
+	{
+		//0节点创建输出文件
+		ofstream createOutFile(outfile.c_str(), ios::binary | ios::out);
+		createOutFile.close();
+	}
+
+	//等待0节点创建完输出文件
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	//打开输出文件
+	ofstream ofs(outfile.c_str(), ios::binary | ios::out | ios::in);
+	if (!ofs.is_open())
+	{
+		cout << "Output file: " << outfile << " open failure" << endl;
+		return 1;
+	}
+
+	writeOutHeader(ofs, opt.ROWS*pos.size(), opt.COLOMNS, pow(2, opt.BITS) - 1);
+	BinaryOutputStream out(ofs.rdbuf(), BigEndian);
+
+	streampos outStartPos = out.tellp();
+	//使用0来初始化
+
+	if (rank == 0)
+	{
+		t5 = MPI_Wtime();
+		cout << "initialize output file:" << t5 - t4 << endl;
+	}
+
+	int rows = opt.ROWS;
+	int cols = opt.COLOMNS;
+	int p = ceil(opt.BITS / 8.0);
+	cout << "rows:"<<rows <<"|cols:"<< cols <<"|p:" <<p << endl;
+
+	//为每个节点分配输出文件位置
+	int allBlockSize = pos.size();
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (rank == 0)
+	{
+		cout << "start decoder" << endl;
+	}
+	for (int i = 0; i < allBlockSize; i++)
+	{
+		if (i%nodes == rank)
+		{
+			in.seekg(pos[i]);
+			out.seekp(outStartPos+streampos(i*p*rows*cols));
+
+			decoder(opt, in, out);
+			cout << "Node:"<<processorName<<" | Rank:"<<rank<<" | component " << i + 1 << "  decoder complete" << endl;
+		}
+
+	}
+
+	if (rank == 0)
+	{
+		t6 = MPI_Wtime();
+		cout << "decoder time:" << t6 - t5 << endl;
+	}
+	ifs.close();
+	ofs.close();
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (rank == 0)
+	{
+		t7 = MPI_Wtime();
+		cout << "decoder total time:" << t7 - t1 << endl;
+	}
+
+	MPI_Finalize();
+	return 0;
+}
 
 int main3(int argc, char **argv)
 {
@@ -170,7 +256,39 @@ int main4()
 	int row = 200;
 	int col = 400;
 	int maxval = 1023;
-	//writeOutHeader(file, row, col, maxval);
+
+	string outfile = "hello.dat";
+
+	ofstream createOutFile(outfile.c_str(), ios::binary | ios::out);
+	createOutFile.close();
+
+
+	//打开输出文件
+	ofstream ofs(outfile.c_str(), ios::binary | ios::out | ios::in);
+	if (!ofs.is_open())
+	{
+		cout << "Output file: " << outfile << " open failure" << endl;
+		return 1;
+	}
+
+	writeOutHeader(ofs, 512, 512, pow(2, 10) - 1);
+	BinaryOutputStream out(ofs.rdbuf(), BigEndian);
+
+	streampos outStartPos = out.tellp();
+	//使用0来初始化
+
+	out.seekp(512, ios::cur);
+	out.write8('A');
+
+	out.seekp(512, ios::cur);
+	out.write8('B');
+	out.write8('C');
+
+	out.seekp(outStartPos);
+	out.write8('Z');
+
+
+	ofs.close();
 	cout << "success" << endl;
 	return 0;
 }
@@ -236,7 +354,7 @@ bool readJLSFrame(BinaryInputStream &in, Options &opt)
 	return true;
 }
 
-void findSofPostion(BinaryInputStream &in, vector<streampos> &pos)
+void findSosPostion(BinaryInputStream &in, vector<streampos> &pos)
 {
 	in.seekg(0, ios::beg);
 	Uint8 tempstr;
